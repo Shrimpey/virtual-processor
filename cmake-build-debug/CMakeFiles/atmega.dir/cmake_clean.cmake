d1fd6efb955f9bdaca6fdd7fe03ebba45bab7cdb
Patryk <wozniakp8@gmail.com>
2018-04-30 21:57:16 +0200

    Adding Cmake from Clion

---
+++ cmake-build-debug/CMakeFiles/atmega.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/atmega.dir/f_add1.c.o"
+  "CMakeFiles/atmega.dir/f_add2.c.o"
+  "CMakeFiles/atmega.dir/f_jmp_memc.c.o"
+  "CMakeFiles/atmega.dir/f_jmp_rel.c.o"
+  "CMakeFiles/atmega.dir/interpreter.c.o"
+  "CMakeFiles/atmega.dir/interrupt.c.o"
+  "CMakeFiles/atmega.dir/main.c.o"
+  "CMakeFiles/atmega.dir/mem_abs.c.o"
+  "atmega.pdb"
+  "atmega"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/atmega.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
