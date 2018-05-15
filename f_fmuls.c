0574c87b8302fda87abbfbc88919af1e639e91c5
Shrimpey <kabat97@gmail.com>
2018-05-15 15:58:57 -0400

    Wincyj testow, poprawa wizualna kodu

--- f_fmuls.c
+++ f_fmuls.c
@@ -5,8 +5,8 @@
 void F_FMULS(void){
 
     //Przedzial rejestrow: R16 <= (R1, R2) <= R23
-    signed char R1 = ((getOpcode() & 0x0070) >> 4) + 16;   //identyfikacja numeru rejestru arg. 1
-    signed char R2 = (getOpcode() & 0x0007) + 16;          //identyfikacja numeru rejestru arg. 2
+    DataType R1 = ((getOpcode() & 0x0070) >> 4) + 16;   //identyfikacja numeru rejestru arg. 1
+    DataType R2 = (getOpcode() & 0x0007) + 16;          //identyfikacja numeru rejestru arg. 2
 
     printf("0x%04X[0x%04X]: FMULS R%d, R%d\n", getPC(), getOpcode(), R1, R2);
 
@@ -14,10 +14,10 @@ void F_FMULS(void){
     DataType R2_pp = ((getRegister(R2) & 0x80) == 0x80) ? (~(getRegister(R2) - 1)) : (getRegister(R2));
 
     CodeType result = R1_pp * R2_pp;    //Obliczenie wyniku
-    result = (((getRegister(R1) & 0x80) ^ (getRegister(R2) & 0x80)) == 0x80) ? (~result + 1) : (result);
+    result = (((getRegister(R1) & 0x80) ^ (getRegister(R2) & 0x80)) == 0x80) ? (~result + 1) : (result);    //Przejscie do U2
 
-    setRegister(0x01, (result << 1) & 0x00FF);       //Ustawienie bitu niskiego
-    setRegister(0x00, ( (result << 1) & 0xFF00) >> 8);       //Ustawienie bitu wysokiego
+    setRegister(0x01, (result << 1) & 0x00FF);              //Ustawienie bitu niskiego
+    setRegister(0x00, ( (result << 1) & 0xFF00) >> 8);      //Ustawienie bitu wysokiego
 
     if((result & 0x8000) == 0x8000){
         setFlagsRegister(FLAG_C);       //Ustawienie flagi przeniesienia
@@ -31,6 +31,6 @@ void F_FMULS(void){
         resetFlagsRegister(FLAG_Z);     //Zresetowanie flagi zero
     }
 
-    setPC(getPC()+1);                                       //zwiekszenie licznika rozkazow
-    addCounter(2);
+    setPC(getPC()+1);                   //zwiekszenie licznika rozkazow
+    addCounter(2);                      //FMULS zajmuje 2 cykle
 }
