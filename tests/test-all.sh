2da5bc9caf810093337c2c7b43912c4568df54e6
mbrzykcy <marcin193c@gmail.com>
2018-05-27 17:22:26 +0200

    Dodano skrypt test-all.sh w folderze tests, który automatyzuje testowanie (patrz readme), zakutalizowano readme, dodano brakujące pliki binarne

---
+++ tests/test-all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ 
+for file in *.s; do
+    [ -e "$file" ] || continue
+    echo "Processing  $file..."
+    $PWD/make-test.sh -f "${file%$".s"}"
+    cd ..
+    $PWD/syko.exe 20 20 zeroPC
+    cd tests
+    cp $PWD/../file_data.bin $PWD/"binary data"/${file%$".s"}_out.bin
+    echo ""
+done
+ 
+cd $PWD/binary\ data/
+ 
+echo "Comparing files..."
+"$PWD"/compare.sh
